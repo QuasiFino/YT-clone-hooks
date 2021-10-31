a1bf2f79eaa5636e8b71e85a04d42d28d7150446
wink-aajlak <aajlak.wink@gmail.com>
2021-10-31 13:50:46 +0530

    video item on click

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../APIs/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
     state = {
         videos: [],
+        selectedVideo: null,
     }
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
@@ -16,14 +18,23 @@ class App extends React.Component {
         this.setState({videos: response.data.items});
     };
 
+    onVideoSelect = (video) => {
+        // console.log('From the App!', video);
+        this.setState({selectedVideo: video});
+    };
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar 
                     onFormSubmit={this.onTermSubmit}
                 />
+                <VideoDetail 
+                    video={this.state.selectedVideo}
+                />
                 <VideoList 
                     videos={this.state.videos}
+                    onVideoSelect={this.onVideoSelect}
                 />
             </div>
         );
