42a123141cc5f83a62febc41dcf83819dd05095c
wink-aajlak <aajlak.wink@gmail.com>
2021-10-31 13:24:07 +0530

    creating video item base

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = (props) => {
+const VideoList = ({ videos }) => {
+    const renderedList = videos.map((video) => {
+        return (
+            <VideoItem 
+                video={video}
+            />
+        );
+    });
+
     return (
-        <div>
-            {props.videos.length}
+        <div className="ui relaxed divided list">
+            {renderedList}
         </div>
     );
 }
